7f75f6af0a7824d36874472ea9f8954b330876cd
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-08-04 14:21:56 -0700

    Update nodetxt.sh

--- apps/nodetxt.sh
+++ apps/nodetxt.sh
@@ -13,7 +13,8 @@
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-cd leafos-files
+clear
+cd docs
 ls
 
 echo "Navigate and create directories with mkdir, cd, ls, and rm."
