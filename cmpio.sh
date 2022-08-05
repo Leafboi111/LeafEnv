d969fdd36d2c309cf1508aacd3bf2db2b1113174
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-08-04 20:15:08 -0700

    Create cmpio.sh

---
+++ cmpio.sh
@@ -0,0 +1,26 @@
+case $1 in
+
+  1)
+    ./os/nodeupd.sh
+    ;;
+  2)
+    ./apps/2048.sh
+    ;;
+  3)
+    ./apps/nodecal.sh
+    ;;
+  4)
+    ./apps/nodecalc.sh
+    ;;
+  5)
+    ./apps/nodeterm.sh
+    ;;
+  6)
+    ./apps/nodetxt.sh
+    ;;
+  7)
+    ./apps/tetris.sh
+    ;;
+  *)
+    ;;
+esac
