6cb9853179db5bf5ebbba84a5b2b318f464a42c3
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-08-05 11:02:16 -0700

    Update nodeterm.sh

--- apps/nodeterm.sh
+++ apps/nodeterm.sh
@@ -20,7 +20,7 @@ function main {
   while true
   do
     currentDate=`date +"%T"`
-    read -p "$(tput setaf 2)nodecli$(tput setaf 7):$(tput setaf 4)$currentDate$(tput setaf 7)\$ " input
+    read -p "$(tput setaf 10)nodeterm$(tput setaf 7):$(tput setaf 4)$currentDate$(tput setaf 7)\$ " input
     $input
   done
 
