bd8054784f991bd84273849887ecbd2b14b68214
Leafboi <110484176+Leafboi111@users.noreply.github.com>
2022-08-06 12:56:51 -0700

    Update nodetxt.sh

--- apps/nodetxt.sh
+++ apps/nodetxt.sh
@@ -1,47 +1,76 @@
-# LeafOS
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation, either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
-
 clear
 cd docs
-tput setaf 45
-ls
 
-tput setaf 226
+str1="Back"
+str2="Add_file"
+str3="Remove_file/directory"
+str4="Add_directory"
+filename=""
 
-read -p "Name of file: " filename
+function addfile {
+  read -p "Name of file: " filename
+  touch $filename
+}
 
-#main file editor
-while true
-do
-  tput setaf 226
-  clear
-  echo "Contents:"
-  cat -n $filename
-  read -p "Mode: read, add, or remove or press enter to exit: " mode
+function removefile {
+  read -p "Name of file: " filename
+  rm -rf $filename
+}
 
-  if [ "$mode" == "read" ];
-  then
-    ls
-  elif [ "$mode" == "add" ];
-  then
-    read -p "$(tput setaf 45)Write to file: " contents
-    echo "$contents" >> "$filename"
-  elif [ "$mode" == "remove" ];
-  then
-    sed -i '$d' $filename
-  else
-    break
-  fi
-done
+function addirectory {
+  read -p "Name of directory: " dirname
+  mkdir $dirname
+}
+
+function selectfile {
+
+  while true
+  do
+    teststr="$str1 $str2 $str3 $str4 $("ls")"
+    clear
+    select input in $teststr
+    do
+      if [ "$input" == "Back" ];
+      then
+        cd ..
+      elif [ "$input" == "Add_file" ];
+      then
+        addfile
+      elif [ "$input" == "Remove_file/directory" ];
+      then
+        removefile
+      elif [ "$input" == "Add_directory" ];
+      then
+        adddirectory
+      else
+        cd $input || filename="$input";
+        return 0
+      fi
+      break
+    done
+  done
+}
+
+function editfile {
+  while true
+  do
+    clear
+    cat -n $filename
+    read -p "add or remove line or press enter to exit: "  addrm
+    if [ "$addrm" == "add" ];
+    then
+      read -p "Contents of file: " contents
+      echo "$contents" >> "$filename"
+    elif [ "$addrm" == "remove" ];
+    then
+      sed -i '$d' $filename
+    else
+      return 0
+    fi
+  done
+}
+
+tput setaf 214
+selectfile
+tput setaf 39
+editfile
