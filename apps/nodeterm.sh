e13fdf6964f972452e3ec3c731be4552303d5993
Alriceee <74868022+Alriceee@users.noreply.github.com>
2022-09-28 08:32:06 -0500

    Create nodeterm.sh

---
+++ apps/nodeterm.sh
@@ -0,0 +1,28 @@
+# nodeterm
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+clear
+
+function main {
+
+  while true
+  do
+    currentDate=`date +"%T"`
+    read -p "$(tput setaf 10)nodeterm$(tput setaf 7):$(tput setaf 4)$currentDate$(tput setaf 7)\$ " input
+    $input
+  done
+
+}
+main
